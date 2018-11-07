541d6d3c7cfd2ab89740f363948cb3d1c00339ea
Jacob Murray <31995858+Jaymur1337@users.noreply.github.com>
2018-11-06 22:12:17 -0500

    Création des fenètres pour la vue

---
+++ ProjetAgrégateur/Controlleur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetAgrégateur
+{
+    class Controlleur
+    {
+        public void NotifierChangerVersNews(Acceuil acceuil)
+        {
+            acceuil.ChangerVersMemes();
+        }
+    }
+}
---
+++ ProjetAgrégateur/Windows/CryptoWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetAgrégateur
+{
+    /// <summary>
+    /// Logique d'interaction pour News.xaml
+    /// </summary>
+    public partial class PokeWindow : Window
+    {
+        public PokeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Acceuil acceuil = new Acceuil();
+            acceuil.Show();
+            this.Close();
+        }
+    }
+}
